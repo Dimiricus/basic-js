1398a417e34ad3de5b4153a23ce81e27d94786f3
Dimiricus <getdimiricus@gmail.com>
2021-09-20 00:50:14 +0300

    feat: add some solutions

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -10,12 +10,80 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * @example
  * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
- * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
+ * repeater('STRING', { repeatTimes: 3, 
+ *                      separator: '**', 
+ *                      addition: 'PLUS', 
+ *                      additionRepeatTimes: 3, 
+ *                      additionSeparator: '00'
+ *                     }
+ *          )
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+export default function repeater(str, options) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  if (typeof str != "string") {
+
+    str = String(str);
+
+  }
+
+  if ( !options.hasOwnProperty('repeatTimes') ) {
+
+    options.repeatTimes = 1;
+
+  }
+
+  if ( !options.hasOwnProperty('separator') ) {
+
+    options.separator = '+';
+
+  }
+
+  if ( !options.hasOwnProperty('addition') ) {
+
+    options.addition = '';
+
+  } else {
+
+    options.addition = String(options.addition);
+
+  }
+
+  if ( !options.hasOwnProperty('additionRepeatTimes') ) {
+
+    options.additionRepeatTimes = 1;
+
+  }
+
+  if ( !options.hasOwnProperty('additionSeparator') ) {
+
+    options.additionSeparator = '|';
+
+  }
+
+  let arr = [];
+
+  let _arr = [];
+
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+
+    arr.push(options.addition);
+
+  }
+
+  arr = arr.join(options.additionSeparator)
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+
+    _arr.push(str + arr);
+
+  }
+
+  _arr = _arr.join(options.separator);
+
+  return _arr;
+
 }
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,103 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(machineType = true) {
+
+    this.machineType = machineType;
+
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+
+  encrypt(message = null, key = null) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
+    if (message === null || key === null) {
+
+      throw new Error("Incorrect arguments!");
+
+    }
+
+    let arr = [];
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+
+      if ( this.alphabet.indexOf( message[i].toUpperCase() ) == -1 ) {
+
+        arr.push( message[i].toUpperCase() );
+
+      } else {
+
+        let _index = this.alphabet.indexOf( key[j % key.length].toUpperCase() );
+
+        let index = ( this.alphabet.indexOf( message[i].toUpperCase() ) + _index + this.alphabet.length ) % this.alphabet.length;
+
+        let str = this.alphabet[index];
+
+        arr.push(str);
+
+        j += 1;
+      }
+
+    }
+
+    if (this.machineType) {
+
+      return arr.join('');
+
+    } else {
+
+      return arr.reverse().join('');
+
+    }
+    
+
+  }
+
+  decrypt(encryptedMessage = null, key = null) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+
+    if (encryptedMessage === null || key === null) {
+
+      throw new Error("Incorrect arguments!");
+
+    }
+
+    let arr = [];
+
+    for (let i = 0, j = 0; i < encryptedMessage.length; i++) {
+
+      if ( this.alphabet.indexOf( encryptedMessage[i].toUpperCase() ) == -1 ) {
+
+        arr.push( encryptedMessage[i].toUpperCase() );
+
+      } else {
+
+        let _index = this.alphabet.indexOf( key[j % key.length].toUpperCase() );
+
+        let index = ( this.alphabet.indexOf( encryptedMessage[i].toUpperCase() ) + -(_index) + this.alphabet.length ) % this.alphabet.length;
+
+        let str = this.alphabet[index];
+
+        arr.push(str);
+
+        j += 1;
+      }
+
+    }
+
+    if (this.machineType) {
+
+      return arr.join('');
+
+    } else {
+
+      return arr.reverse().join('');
+
+    }
+
   }
 }
